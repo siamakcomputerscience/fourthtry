22800f05e2eeef184c97b55a2275d252e8e707a3
siamakcomputerscience <siamakcomputerscience@gmail.com>
2015-07-24 22:39:31 -0400

    ssss

--- proj3/src/proj3/Main3.java
+++ proj3/src/proj3/Main3.java
@@ -4,7 +4,7 @@ public class Main3 {
 
 	public static void main(String[] args) {
 		System.out.println("Main 3236666");
-		System.out.println("dom dom");
+		System.out.println("dom dom 5555");
 
 	}
 
